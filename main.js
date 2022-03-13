b1750b0b2d48775964f8690e584da9603b1ac4d4
jsonggan <1006283@mymail.sutd.edu.sg>
2022-03-13 21:07:18 +0800

    k

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 "use strict";
+
+function decrease_number() {
+  let current_value = document.getElementById("number_button").innerHTML;
+  let new_value = current_value - 1;
+  if (new_value >= 0) {
+    document.getElementById("number_button").innerHTML = new_value;
+  }
+}
